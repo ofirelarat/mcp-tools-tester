dbd8968e013bf9e6770eecee533e5e036c3e2473
ofirelarat <96ofir11@gmail.com>
2025-10-31 15:18:09 +0200

    remove not relevant

--- ollama/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Ollama container...
-docker build -t local-ollama ./ollama
-docker run -d --name ollama ^
-  -p 11434:11434 ^
-  -v ollama-models:/root/.ollama ^
-  local-ollama
-echo Ollama running on http://localhost:11434
